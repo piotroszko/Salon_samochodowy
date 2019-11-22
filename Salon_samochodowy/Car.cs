29b4b027e18d57e5e3bf6f1de097bef22b6deae5
Piotr Roszkowski <151183@student.uwm.edu.pl>
2019-11-22 12:50:17 +0100

    Dokonczenie Car class, Dodanie podstawowej formy zamowienia.

--- Salon_samochodowy/Car.cs
+++ Salon_samochodowy/Car.cs
@@ -13,20 +13,22 @@ namespace Salon_samochodowy
         List<string> nazwaModelu = new List<string>();
         List<string> listaKolorow = new List<string>(); // #0000000,#C0C0C0, itp
         List<bool> czyWSprzedazy = new List<bool>();
-        List<Wyposazecznie_Car> listaMozliwegoWyposazenia = new List<Wyposazecznie_Car>();
+        List<Wyposazenie_Car> listaMozliwegoWyposazenia = new List<Wyposazenie_Car>();
+        List<int> cenaPodstawowa = new List<int>();
 
         public Car(string nazwaBazySamochodow)
         {
             this.nazwaBazySamochodow = nazwaBazySamochodow;
         }
-        public void DodajModel(string nazwaModelu,string _listaKolorow,bool _czyWSprzedazy, Wyposazecznie_Car mozliweWyposazenie)
+        public void DodajModel(string nazwaModelu,string _listaKolorow,bool _czyWSprzedazy, int cena, Wyposazenie_Car mozliweWyposazenie)
         {
             this.nazwaModelu.Add(nazwaModelu);
             this.listaKolorow.Add(_listaKolorow);
             this.czyWSprzedazy.Add(_czyWSprzedazy);
             this.listaMozliwegoWyposazenia.Add(mozliweWyposazenie);
+            this.cenaPodstawowa.Add(cena);
         }
-        public void EdytujModel(string _nazwaModelu, string _listaKolorow, bool _czyWSprzedazy, Wyposazecznie_Car mozliweWyposazenie)
+        public void EdytujModel(string _nazwaModelu, string _listaKolorow, bool _czyWSprzedazy,int cena, Wyposazenie_Car mozliweWyposazenie)
         {
             int id = IDModelu(_nazwaModelu);
             if (id != -1)
@@ -34,12 +36,13 @@ namespace Salon_samochodowy
                 this.listaKolorow[id] = _listaKolorow;
                 this.czyWSprzedazy[id] = _czyWSprzedazy;
                 this.listaMozliwegoWyposazenia[id] = mozliweWyposazenie;
+                this.cenaPodstawowa[id] = cena;
             } else
             {
-                MessageBox.Show("Nie znaleziono podanego modelu!");
+                MessageBox.Show("Nie znaleziono podanego modelu samochodu!");
             }
         }
-        private int IDModelu (string _nazwaModelu)
+        public int IDModelu (string _nazwaModelu)
         {
             int i = 0;
             foreach (string nazwa in this.nazwaModelu)
@@ -53,21 +56,57 @@ namespace Salon_samochodowy
             }
             return -1;
         }
-        public string[] Kolory (string _nazwaModelu)
+        public List<string> WygenerujListeKolorowDlaModelu (string _nazwaModelu)
         {
             int id = IDModelu(_nazwaModelu);
             List<string> kolory = new List<string>();
             if (id != -1)
             {
-                
+                string[] tmpStr = this.listaKolorow[id].Split(',');
+                foreach (string kolor in tmpStr)
+                {
+                    kolory.Add(kolor);
+                }
+                return kolory;
             }
             else
             {
                 return kolory;
             }
         }
+        public bool CzyWSprzedazyModel(string _nazwaModelu)
+        {
+            int id = IDModelu(_nazwaModelu);
+            if (id != -1)
+            {
+                if (this.czyWSprzedazy[id] == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int CenaPodstawowaModelu(string _nazwaModelu)
+        {
+            int id = IDModelu(_nazwaModelu);
+            if (id != -1)
+            {
+                return this.cenaPodstawowa[id];
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
-    class Wyposazecznie_Car
+    class Wyposazenie_Car
     {
         public bool opcja1; //czy 1 opcja wyposazenia jest mozliwa w tym modelu ? itp
         public bool opcja2;
---
+++ Salon_samochodowy/zamowienie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon_samochodowy
+{
+    class zamowienie
+    {
+        public string model;
+        public string kolor;
+        public Wyposazenie wyposazenie;
+        public int cenaPodstawowa;
+        public int cenaKoncowa;
+        public Klient klient;
+    }
+    class Wyposazenie
+    {
+        public bool opcja1;
+        public bool opcja2;
+        public bool opcja3;
+        public bool opcja4;
+        public string kolor;
+    }
+}
